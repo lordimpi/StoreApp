683d9e6213ffb883b8738f01484956168d8efb68
Santiago Acuña <snt-26@hotmail.com>
2021-07-27 03:13:34 -0500

    Implementing DeletDialog in ProductsIndex

--- StoreApp/Client/Program.cs
+++ StoreApp/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Radzen;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -18,6 +19,7 @@ namespace StoreApp.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<DialogService>();
 
             await builder.Build().RunAsync();
         }
